12d164b835ae6ba5668776a9abf4692e0e81487d
laila <lailaalabdullah374@gmail.com>
2026-07-21 11:46:29 +0300

    feat(idetity): add global exception filter, fixed validation pipe and app bootstrap

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersistenceModule } from './infrastructure/persistence/persistence.module';
+import { CatalogModule } from './interface/catalog/catalog.module';
+import { IdentityModule } from './interface/identity/identity.module';
+import { OrganizationModule } from './interface/organization/organization.module';
+import { WorkflowModule } from './interface/workflow/workflow.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PersistenceModule,
+    CatalogModule,
+    IdentityModule,
+    OrganizationModule,
+    WorkflowModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
